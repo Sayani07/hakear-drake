d708a5384663c57965837fb5697b605134784c66
Sayani07 <Sayani.Gupta@monash.edu>
2020-09-17 16:52:40 +1000

    mpd and mmpd updated

--- R/mmpd.R
+++ R/mmpd.R
@@ -10,6 +10,22 @@
 ##' @export
 mmpd <- function(normx_data, nperm = 2000) {
 
-  NULL
+  panel_facet <- normx_data %>% select(id_facet, norm_x) 
+  
+  nperm_data_1 <- bind_cols(perm_id = 1, perm_data = panel_facet) %>% 
+    rename("perm_data" ="norm_x")
+  
+nperm_data_n <- (2:nperm) %>%
+    map_df(function(i) {
+      perm_data <- sample(panel_facet$norm_x, size = nrow(panel_row), replace = TRUE)
+      bind_cols(perm_id = i, id_facet = k, perm_data = perm_data)
+    })
+
+perm_facet_data <- bind_rows(nperm_data_1, nperm_data_n)
+
+perm_facet_data %>% 
+  summarise(mean_perm = mean(perm_data),
+            sd_perm = sd(perm_data)) %>% 
+  mutate(mmpd = (median(nperm_data_1$perm_data) - mean_perm)/sd_perm) %>% pull(mmpd) %>% round(3)
 
 }
--- R/mpd.R
+++ R/mpd.R
@@ -9,20 +9,32 @@
 ##' @author Sayani07
 ##' @export
 mpd <- function(distance_panel_data, nperm = 2000) {
-
-
-
-nperm_data_n <- (1:nfacet) %>%
+  
+nfacet <- nrow(distance_panel_data)  
+  
+nperm_data_n <- (seq_len(nfacet)) %>%
   map_df(function(k) {
-  nperm_data_1 <- bind_cols(perm_id = 1, perm_data = distance_panel_data) 
+    
+  panel_row <- distance_panel_data %>%
+    magrittr::extract(k,)%>%
+    tidyr::pivot_longer(cols = -1, names_to = "j") %>% 
+    select(id_facet, value) 
+  
+  nperm_data_1 <- bind_cols(perm_id = 1, perm_data = panel_row) %>% 
+    rename("perm_data" = "value")
   nperm_data_n <- (2:nperm) %>%
     map_df(function(i) {
-      perm_data <- sample(sim_data, size = length(sim_data), replace = TRUE)
-      bind_cols(perm_id = i, perm_data = perm_data)
+      perm_data <- sample(panel_row$value, size = nrow(panel_row), replace = TRUE)
+      bind_cols(perm_id = i, id_facet = k, perm_data = perm_data)
     })
   })
   
-  bind_rows(nperm_data_1, nperm_data_n)
-  
-
+ perm_facet_data <- bind_rows(nperm_data_1, nperm_data_n)
+ 
+ perm_facet_data %>%
+   group_by(id_facet) %>% 
+   summarise(max = max(perm_data),
+     mean_perm = mean(perm_data),
+             sd_mean = sd(perm_data)) %>% 
+   mutate(norm_x = (max - mean_perm)/sd_mean)
 }
