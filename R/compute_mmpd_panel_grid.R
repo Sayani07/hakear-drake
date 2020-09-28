61d3cad66d40cb57360fb65b8a5c608f1bacf22a
Sayani Gupta <Sayani.Gupta@monash.edu>
2020-09-28 23:08:42 +1000

    nperm value fixed

--- R/compute_mmpd_panel_grid.R
+++ R/compute_mmpd_panel_grid.R
@@ -11,8 +11,7 @@
 ##' @author Sayani Gupta
 ##' @export
 compute_mmpd_panel_grid <- function(sim_null_orig, quantile_prob = seq(0.01,
-                                    0.99, 0.01), dist_ordered = TRUE, nperm =
-                                    200) {
+                                    0.99, 0.01), dist_ordered = TRUE, nperm = 20) {
 
   nx <- sim_null_orig %>% distinct(nx) %>% .$nx
   nfacet <- sim_null_orig %>% distinct(nfacet) %>% .$nfacet
@@ -23,8 +22,8 @@ compute_mmpd_panel_grid <- function(sim_null_orig, quantile_prob = seq(0.01,
         map_df(function(j){
           
   mmpd <- sim_null_orig %>% 
-  dplyr::filter(nx == i & nfacet == j) %>%  
-  compute_mmpd_panel(quantile_prob, dist_ordered, nperm=200)
+  dplyr::filter(nx == i, nfacet == j) %>%  
+  compute_mmpd_panel(quantile_prob, dist_ordered, nperm = nperm)
   
   bind_cols(nx = i, nfacet = j, mmpd = mmpd)
         
