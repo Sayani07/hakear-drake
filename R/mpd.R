d3f3a71c40f855e92125544f83ccb6883e5fd8a8
Sayani Gupta <Sayani.Gupta@monash.edu>
2020-09-28 23:09:54 +1000

    grom map_df to apply

--- R/mpd.R
+++ R/mpd.R
@@ -8,8 +8,11 @@
 ##' @return
 ##' @author Sayani07
 ##' @export
-mpd <- function(distance_panel_data, nperm = 2000) {
+
+source("R/shuffle_data.R")
+mpd <- function(sim_panel_data, distance_panel_data, nperm = 2000) {
   nfacet <- nrow(distance_panel_data)
+
 
   nperm_data_facet <- (seq_len(nfacet)) %>%
     map_df(function(k) {
@@ -17,22 +20,28 @@ mpd <- function(distance_panel_data, nperm = 2000) {
       panel_row <- distance_panel_data %>%
         magrittr::extract(k, ) %>%
         tidyr::pivot_longer(cols = -1, names_to = "j") %>% select(id_facet, value)
-
+      
       nperm_data_1 <- bind_cols(perm_id = 1, perm_data = panel_row) %>%
         rename("perm_data" = "value")
-
-      nperm_data_n <- (2:nperm) %>%
-        map_df(function(i) {
+      
+      nperm_data <- data.frame(2:nperm)
+      #nperm_data_n <- (2:nperm) %>%
+      nperm_data_n <- apply(nperm_data, 2,  function(i) {
+          z = NULL
           
-          perm_data <- sample(panel_row$value, size = nrow(panel_row), replace = TRUE)
+          perm_data <- sim_panel_data %>% 
+            dplyr::filter(id_facet==k) %>% 
+          shuffle_data() %>% 
+            compute_quantiles() %>% 
+            distance_panel() %>% 
+            rename("perm_data" = "...2")
           
-          bind_cols(perm_id = i, id_facet = k, perm_data = perm_data)
+          z = bind_cols(perm_id = i, id_facet = k, perm_data = perm_data$perm_data)
           
         })
       
-      bind_rows(nperm_data_1, nperm_data_n)
+      bind_rows(nperm_data_1, nperm_data_n$X2.nperm)
     })
-
 
   nperm_data_facet %>%
     group_by(id_facet) %>%
@@ -44,3 +53,4 @@ mpd <- function(distance_panel_data, nperm = 2000) {
     mutate(norm_x = if_else(sd_perm !=0, 
                             (max - mean_perm) / sd_perm,max))
 }
+
