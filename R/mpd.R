6f6fbdc956be4d7c3ea1f072138b2d0669a98fdf
Sayani Gupta <Sayani.Gupta@monash.edu>
2020-09-30 11:37:08 +1000

    changed to apply from map functions to make faster

--- R/mpd.R
+++ R/mpd.R
@@ -13,35 +13,35 @@ source("R/shuffle_data.R")
 mpd <- function(sim_panel_data, distance_panel_data, nperm = 2000) {
   nfacet <- nrow(distance_panel_data)
 
-
-  nperm_data_facet <- (seq_len(nfacet)) %>%
-    map_df(function(k) {
-      
+  # nperm_data_facet <- lapply(seq_len(nfacet),
+  #                           function(k) {
+  #     
       panel_row <- distance_panel_data %>%
-        magrittr::extract(k, ) %>%
         tidyr::pivot_longer(cols = -1, names_to = "j") %>% select(id_facet, value)
       
-      nperm_data_1 <- bind_cols(perm_id = 1, perm_data = panel_row) %>%
+      nperm_data_1 <- bind_cols(perm_data = panel_row) %>%
         rename("perm_data" = "value")
       
       nperm_data <- data.frame(2:nperm)
       #nperm_data_n <- (2:nperm) %>%
-      nperm_data_n <- apply(nperm_data, 2,  function(i) {
+      nperm_data_n <- lapply(2:nperm, 
+                            function(i) {
           z = NULL
           
           perm_data <- sim_panel_data %>% 
-            dplyr::filter(id_facet==k) %>% 
+            dplyr::group_by(id_facet) %>% 
           shuffle_data() %>% 
             compute_quantiles() %>% 
             distance_panel() %>% 
-            rename("perm_data" = "...2")
+            pivot_longer(-1, names_to = "dist", 
+                         values_to = "perm_data") %>% select(-dist)
+            #rename("perm_data" = "...2")
           
-          z = bind_cols(perm_id = i, id_facet = k, perm_data = perm_data$perm_data)
-          
+          #z = bind_cols(perm_id = i, id_facet = k, perm_data = perm_data$perm_data)
+           #z = bind_cols(perm_data = perm_data$perm_data,
         })
       
-      bind_rows(nperm_data_1, nperm_data_n$X2.nperm)
-    })
+      nperm_data_facet <- bind_rows(nperm_data_n, nperm_data_n, .id = "perm_id")
 
   nperm_data_facet %>%
     group_by(id_facet) %>%
