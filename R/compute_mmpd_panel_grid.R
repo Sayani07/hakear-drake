3bef58692f06d70beeb84232d2f0e6da12845574
Sayani Gupta <Sayani.Gupta@monash.edu>
2020-10-15 12:01:23 +1100

    will be gone - mcapply directly in the plan after splitting the main data

--- R/compute_mmpd_panel_grid.R
+++ R/compute_mmpd_panel_grid.R
@@ -3,33 +3,48 @@
 ##' .. content for \details{} ..
 ##'
 ##' @title
-##' @param sim_null_orig
+##' @param sim_null_split
 ##' @param quantile_prob
 ##' @param dist_ordered
 ##' @param nperm
 ##' @return
 ##' @author Sayani Gupta
 ##' @export
-compute_mmpd_panel_grid <- function(sim_null_orig, quantile_prob = seq(0.01,
+compute_mmpd_panel_grid <- function(sim_null_split, quantile_prob = seq(0.01,
                                     0.99, 0.01), dist_ordered = TRUE, nperm = 20) {
 
-  nx <- sim_null_orig %>% distinct(nx) %>% .$nx
-  nfacet <- sim_null_orig %>% distinct(nfacet) %>% .$nfacet
+  l = length(sim_null_split)
   
-  (nx) %>% 
-    map_df(function(i){
-      (nfacet) %>% 
-        map_df(function(j){
-          
-  mmpd <- sim_null_orig %>% 
-  dplyr::filter(nx == i, nfacet == j) %>%  
-  compute_mmpd_panel(quantile_prob, 
-                     dist_ordered, 
-                     nperm = nperm)
-  
-  bind_cols(nx = i, nfacet = j, mmpd = mmpd)
-        
+  mclapply(seq_len(l), function(i){
+    compute_mmpd_panel(sim_null_split[[i]],
+                       nperm = 20, 
+                       dist_ordered = TRUE)
   })
-    })
-
 }
+  
+  
+  
+  
+#   nx <- sim_null_orig %>% 
+#     bind_rows() %>% 
+#       distinct(nx) %>% .$nx
+#   
+#   nfacet <- sim_null_orig %>% distinct(nfacet) %>% .$nfacet
+#   
+#   (nx) %>% 
+#     map_df(function(i){
+#       (nfacet) %>% 
+#         map_df(function(j){
+#           
+#   mmpd <- sim_null_orig %>% 
+#   dplyr::filter(nx == i, nfacet == j) %>%  
+#   compute_mmpd_panel(quantile_prob, 
+#                      dist_ordered, 
+#                      nperm = nperm)
+#   
+#   bind_cols(nx = i, nfacet = j, mmpd = mmpd)
+#         
+#   })
+#     })
+# 
+# }
