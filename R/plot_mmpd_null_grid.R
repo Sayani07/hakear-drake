1e3c2b28e8ddeba00592c08153b8ae2fced9d2ec
Sayani Gupta <Sayani.Gupta@monash.edu>
2020-10-15 17:14:11 +1100

    vertical line at the right place

--- R/plot_mmpd_null_grid.R
+++ R/plot_mmpd_null_grid.R
@@ -3,26 +3,29 @@
 ##' .. content for \details{} ..
 ##'
 ##' @title
-##' @param mmpd_dist_null_grid
-##' @param mmpd_null_orig
+##' @param mmpd_dist_null_data
+##' @param mmpd_null_orig_data
 ##' @return
 ##' @author Sayani Gupta
 ##' @export
-plot_mmpd_null_grid <- function(mmpd_dist_null_grid, mmpd_null_orig) {
+plot_mmpd_null_grid <- function(mmpd_dist_null_data, mmpd_null_orig_data) {
   
-  compute_p_value <- left_join(mmpd_dist_null_grid, mmpd_null_orig,
-                               by = c("nx", "nfacet")) %>% 
+  joined_data <- left_join(mmpd_dist_null_data, mmpd_null_orig_data,
+                               by = c("nx", "nfacet"))
+  
+  
+  compute_p_value <- joined_data %>% 
     group_by(nx, nfacet) %>% 
     summarise(p_value = sum(if_else(abs(mmpd.x)>abs(mmpd.y),1,0))/n(), .groups = 'drop')
 
   ggplot() + 
-    geom_histogram(data = mmpd_dist_null_grid, aes(x = mmpd))  + 
-    geom_vline(data = mmpd_null_orig, aes(xintercept = mmpd),colour = "red") +
+    geom_histogram(data = mmpd_dist_null_data, aes(x = mmpd))  + 
+    geom_vline(data = mmpd_null_orig_data, aes(xintercept = mmpd),colour = "red") +
     geom_text(data = compute_p_value, size = 3,  
               aes(x = -Inf,
                   y =  Inf,
-                  label = paste("p-value:",p_value),
-                  hjust   = 0,
-                  vjust   = 1)) + 
+                  label = paste("p-value:",p_value,
+                                hjust   = 0,
+                                vjust   = 1))) + 
     facet_grid(nx ~ nfacet)
 }
