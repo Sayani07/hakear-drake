d70b6d788555eba5218f8edc94606cae9e5d731f
Sayani Gupta <Sayani.Gupta@monash.edu>
2020-10-28 09:38:45 +1100

    sim_dist made into a function to take into account diff sim study

--- R/sim_panel.R
+++ R/sim_panel.R
@@ -12,21 +12,22 @@
 ##' @export
 
 sim_panel <- function(nx = 2, nfacet = 3, 
-                      ntimes = 5, sim_dist =
-                        distributional::dist_normal(5, 10)) {
-  if(length(sim_dist)==1)
-  {
-    sim_dist = rep(sim_dist, nx*nfacet)
-  }
+                      ntimes = 5, 
+                      sim_dist =
+                      sim_varf_dist1) {
+ 
+  sim_dist_data = sim_dist(nx, nfacet)
+  
   id_x <- rep(seq_len(nx), nfacet)
   id_facet <- rep(seq_len(nfacet), each = nx)
   
   sim_data2  <- tibble(id_x, 
                        id_facet,
-                       sim_dist) %>% 
+                       sim_dist_data) %>% 
     group_by(id_facet, 
              id_x) %>%
-    summarise(sim_data = generate(sim_dist, times = ntimes), .groups = 'drop') %>% unnest(sim_data)
+    summarise(sim_data = generate(sim_dist_data, 
+                                  times = ntimes), .groups = 'drop') %>% unnest(sim_data)
   
   sim_data2 %>% mutate(nfacet = nfacet, nx = nx) %>% 
     select(nfacet, nx, id_facet, id_x, sim_data)%>% 
