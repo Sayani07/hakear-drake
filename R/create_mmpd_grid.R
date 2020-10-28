b5e6719ab04aed89b8142b50443cb778a6d719b6
Sayani Gupta <Sayani.Gupta@monash.edu>
2020-10-29 10:43:18 +1100

    in a function format

--- R/create_mmpd_grid.R
+++ R/create_mmpd_grid.R
@@ -3,30 +3,32 @@
 ##' .. content for \details{} ..
 ##'
 ##' @title
+##' @param sim_dist
 ##' @param range_nfacet
 ##' @param range_nx
-##' @param ntimes_val
-##' @param nperm_val
-##' @param nsim_val
-##' @param quantile_prob_vec
-##' @param dist_ordered_val
+##' @param ntimes
+##' @param nperm
+##' @param nsim
+##' @param quantile_prob
+##' @param dist_ordered
 ##' @return
 ##' @author Sayani Gupta
 ##' @export
-create_mmpd_grid <- function(range_nfacet = range_nfacet_vec, 
-                      range_nx = range_nx_vec, 
-                      ntimes = ntimes_val, 
-                      nperm = nperm_val,
-                      nsim = nsim_val, 
-                      quantile_prob = quantile_prob_vec, 
-                      dist_ordered = dist_ordered_val,
-                      sim_dist = sim_null_dist1) {
+create_mmpd_grid <- function(
+  sim_dist = sim_null_normal,
+  range_nfacet = 2:4, 
+                      range_nx = 2:4, 
+                      ntimes = 50, 
+                      nperm = 10,
+                      nsim = 100, 
+                      quantile_prob = swq(0.01, 0.99, 0.01), 
+                      dist_ordered = TRUE) {
 
   
-  sim_null_orig  = sim_panel_grid (range_nx = range_nx_vec,                             range_nfacet =
-                                     range_nfacet_vec,
-                                   ntimes = ntimes_val,
-                                   sim_dist = sim_null_dist1)
+  sim_null_orig  = 
+    sim_panel_grid (range_nx,                           range_nfacet,
+                    ntimes,
+                    sim_dist)
   
   sim_null_split = sim_null_orig %>% 
     group_by(nx, nfacet) %>% 
@@ -37,23 +39,24 @@ create_mmpd_grid <- function(range_nfacet = range_nfacet_vec,
     sim_null_split,
     function(x){
       compute_mmpd_panel(x,
-                         quantile_prob = quantile_prob_vec,
-                         dist_ordered = dist_ordered_val,
-                         nperm = nperm_val
+                         quantile_prob,
+                         dist_ordered,
+                         nperm
       )
     }))
   
   # distribution of mmpd
-  mmpd_null_dist = suppressMessages(lapply(
+mmpd_null_dist = 
+  suppressMessages(lapply(
     sim_null_split,
     function(x){
-      replicate(nsim_val,
+      replicate(nsim,
                 {compute_mmpd_panel(shuffle_x_for_each_facet(x),
-                                    quantile_prob = quantile_prob_vec,
-                                    dist_ordered = dist_ordered_val,
-                                    nperm = nperm_val
+                                    quantile_prob = quantile_prob,
+                                    dist_ordered = dist_ordered,
+                                    nperm
                 )
-                  })
+                })
     }))
   
 sim_null_neat  = sim_null_orig %>% group_by(nfacet, nx) %>% nest()  
@@ -71,6 +74,7 @@ sim_null_neat  = sim_null_orig %>% group_by(nfacet, nx) %>% nest()
   
   joined_data = left_join(mmpd_dist_null_data, mmpd_null_orig_data,
                           by = c("nx", "nfacet"))
+  
 
   # compute_p_value = joined_data %>%
   #   group_by(nx, nfacet) %>%
