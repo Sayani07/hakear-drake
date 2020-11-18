51d660eb2e0e64a082404332a9ff724ab7e1353f
Sayani Gupta <Sayani.Gupta@monash.edu>
2020-11-19 02:06:12 +1100

    update compute quantiles

--- R/compute_quantiles.R
+++ R/compute_quantiles.R
@@ -1,31 +1,30 @@
-##' .. content for \description{} (no empty lines) ..
+##' @title compute quantiles of data across categories
 ##'
-##' .. content for \details{} ..
-##'
-##' @title
 ##' @param sim_panel_data
 ##' @param quantile_prob
+##'
 ##' @return
 ##' @author Sayani07
 ##' @export
-compute_quantiles <- function(sim_panel_data, quantile_prob = seq(0.01, 0.99,0.01)) {
-
-  sim_panel_data = sim_panel_data %>% unnest(data)
+compute_quantiles <- function(sim_panel_data, quantile_prob = seq(0.01, 0.99, 0.01)) {
   facet <- unique(sim_panel_data$id_facet)
   nfacet <- length(facet)
-  
-  #quantile_prob <- seq(0.01, 0.05, 0.01)
-  
-  sim_facet_data <- sim_panel_data %>% 
-    # for each group find quantiles
-    group_by(id_facet, id_x) %>% 
-    summarize(list_data = list(sim_data), 
-              sim_data_quantile = quantile(unlist(list_data), quantile_prob), .groups = 'drop') %>% ungroup() %>% select(-list_data) %>% 
-    nest(sim_data_quantile = sim_data_quantile)
-    # put each x on the columns so that pairwise distance could be computed
-    # pivot_wider(id_cols = c(1,2,4),
-    #             names_from = id_x,
-    #             values_from = sim_data_quantile,
-    #             values_fn = list(sim_data_quantile = list)) %>%   
 
+  # quantile_prob <- seq(0.01, 0.05, 0.01)
+
+  sim_facet_data <- sim_panel_data %>%
+    # for each group find quantiles
+    group_by(id_facet, id_x) %>%
+    summarize(
+      list_data = list(sim_data),
+      sim_data_quantile = quantile(unlist(list_data), quantile_prob), .groups = "drop"
+    ) %>%
+    ungroup() %>%
+    select(-list_data) %>%
+    nest(sim_data_quantile = sim_data_quantile)
+  # put each x on the columns so that pairwise distance could be computed
+  # pivot_wider(id_cols = c(1,2,4),
+  #             names_from = id_x,
+  #             values_from = sim_data_quantile,
+  #             values_fn = list(sim_data_quantile = list)) %>%
 }
